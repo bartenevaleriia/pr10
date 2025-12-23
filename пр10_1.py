bd7607efd7b6d0ed4b0652adffbb5dd172fbea69
bartenevaleriia <bartenevalera9@gmail.com>
2025-12-23 20:40:47 +0300

    Add files via upload

---
+++ пр10_1.py
@@ -0,0 +1,9 @@
+while True:
+    pin_code = input('Введите пин-код: ')
+
+    if pin_code == '4590':
+        print('Доступ разрешен')
+        break
+
+    else:
+        print('Ошибка. Попробуйте еще раз')
